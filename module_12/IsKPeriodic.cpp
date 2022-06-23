b6d89aba1b7f672c90fb00d9ebee0f1f1770b09d
re <re@MacBook-Air-re.local>
2022-06-23 13:21:56 +0300

    fix: compliance with the task | add: more tests

--- module_12/IsKPeriodic.cpp
+++ module_12/IsKPeriodic.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <string>
 
-int IsKPeriodic(const std::string& str) {
+int findPeriodic(const std::string& str) {
 
 	int strSize = str.size();
 	int posEnd = 0;
@@ -27,14 +27,34 @@ int IsKPeriodic(const std::string& str) {
 	return ++posEnd;
 }
 
+bool isKPeriodic(const std::string& str, const int& k) {
+	if (k < 1) {
+		std::cout << "k должно быть больше 0!";
+		return false;
+	}
+	
+	if (str.size() % k != 0) return false;
+
+	int periods = str.size() / k;
+	auto part = str.substr(0, k);
+
+	for (int i = 0; i < periods; ++i)
+		if (str.substr(k * i, k) != part)
+			return false;
+	return true;
+}
+
 int main() {
-	std::cout << "Кратность строки \"a\" = " << IsKPeriodic("a") << std::endl << std::endl;
-	std::cout << "Кратность строки \"aaaaaaaaaaaaaaaa\" = " << IsKPeriodic("aaaaaaaaaaaaaaaa") << std::endl << std::endl;
+	std::cout << "Минимальная кратность строки \"a\" = " << findPeriodic("a") << std::endl << std::endl;
+	std::cout << "Минимальная кратность строки \"aaaaaaaaaaaaaa\" = " << findPeriodic("aaaaaaaaaaaaaa") << std::endl << std::endl;
 
-	std::cout << "Кратность строки \"abcabcdabcabcd\" = " << IsKPeriodic("abcabcabcabc") << std::endl << std::endl;
-	std::cout << "Кратность строки \"abcabcabcabcd\" = " << IsKPeriodic("abcabcabcabcd") << std::endl << std::endl;
-	std::cout << "Кратность строки \"abcabcdabcabcd\" = " << IsKPeriodic("abcabcdabcabcd") << std::endl << std::endl;
+	std::cout << "Минимальная кратность строки \"abcabcdabcabcd\" = " << findPeriodic("abcabcabcabc") << std::endl << std::endl;
+	std::cout << "Минимальная кратность строки \"abcabcabcabcd\" = "  << findPeriodic("abcabcabcabcd") << std::endl << std::endl;
+	std::cout << "Минимальная кратность строки \"abcabcdabcabcd\" = " << findPeriodic("abcabcdabcabcd") << std::endl << std::endl << std::endl;
 
-	std::cout << "Кратность строки \"\" = " << IsKPeriodic("") << std::endl << std::endl;
+	std::cout << "Строка \"abcabcdabcabc\" кратна 3-м ? = " << (isKPeriodic("abcabcabcabc", 3) ? "да" : "нет") << std::endl << std::endl;
+	std::cout << "Строка \"abcabcdabcabc\" кратна 4-м ? = " << (isKPeriodic("abcabcabcabc", 4) ? "да" : "нет") << std::endl << std::endl;
+	std::cout << "Строка \"abcabcdabcabc\" кратна 6-и ? = " << (isKPeriodic("abcabcabcabc", 6) ? "да" : "нет") << std::endl << std::endl;
+
 	return 0;
 }
